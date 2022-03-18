48adb78a2a0bdc0dcd015ff0ad7e55b03203c20a
FlorianWoelki <FlorianWoelki@gmx.de>
2022-03-18 11:09:13 +0100

    feat: add certificate viewer

---
+++ lib/components/lists/continue_watching_list.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../model/course.dart';
+import '../cards/continue_watching_card.dart';
+
+class ContinueWatchingList extends StatefulWidget {
+  const ContinueWatchingList({Key? key}) : super(key: key);
+
+  @override
+  State<ContinueWatchingList> createState() => _ContinueWatchingListState();
+}
+
+class _ContinueWatchingListState extends State<ContinueWatchingList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: continueWatchingCourses.map((course) {
+        var index = continueWatchingCourses.indexOf(course);
+        return Container(
+          width: 7.0,
+          height: 7.0,
+          margin: const EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: currentPage == index
+                ? const Color(0xFF0971FE)
+                : const Color(0xFFA6AEBD),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 200.0,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return ContinueWatchingCard(
+                course: continueWatchingCourses[index],
+              );
+            },
+            itemCount: continueWatchingCourses.length,
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+            controller: PageController(
+              initialPage: 0,
+              viewportFraction: 0.75,
+            ),
+          ),
+        ),
+        updateIndicators(),
+      ],
+    );
+  }
+}
