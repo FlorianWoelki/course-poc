655f26f7eb866328004df83f145244c2a49a941f
FlorianWoelki <FlorianWoelki@gmx.de>
2022-03-28 11:32:11 +0200

    feat(comp): add explore course illustration url to firebase storage url

--- lib/components/cards/explore_course_card.dart
+++ lib/components/cards/explore_course_card.dart
@@ -1,12 +1,38 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 import '../../model/course.dart';
 
-class ExploreCourseCard extends StatelessWidget {
+class ExploreCourseCard extends StatefulWidget {
   const ExploreCourseCard({required this.course, Key? key}) : super(key: key);
 
   final Course course;
+
+  @override
+  State<ExploreCourseCard> createState() => _ExploreCourseCardState();
+}
+
+class _ExploreCourseCardState extends State<ExploreCourseCard> {
+  final _storage = FirebaseStorage.instance;
+  String? illustrationURL;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadIllustration();
+  }
+
+  void _loadIllustration() {
+    _storage
+        .ref("illustrations/${widget.course.illustration}")
+        .getDownloadURL()
+        .then((url) {
+      setState(() {
+        illustrationURL = url;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +43,7 @@ class ExploreCourseCard extends StatelessWidget {
         child: Container(
           height: 120.0,
           width: 280.0,
-          decoration: BoxDecoration(gradient: course.background),
+          decoration: BoxDecoration(gradient: widget.course.background),
           child: Padding(
             padding: const EdgeInsets.only(left: 32.0),
             child: Row(
@@ -27,10 +53,11 @@ class ExploreCourseCard extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(course.courseSubtitle, style: kCardSubtitleStyle),
+                      Text(widget.course.courseSubtitle,
+                          style: kCardSubtitleStyle),
                       const SizedBox(height: 6.0),
                       Text(
-                        course.courseTitle,
+                        widget.course.courseTitle,
                         style: kCardTitleStyle,
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
@@ -41,11 +68,13 @@ class ExploreCourseCard extends StatelessWidget {
                 Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Image.asset(
-                      "asset/illustrations/${course.illustration}",
-                      fit: BoxFit.cover,
-                      height: 100.0,
-                    ),
+                    illustrationURL == null
+                        ? Container()
+                        : Image.network(
+                            illustrationURL!,
+                            fit: BoxFit.cover,
+                            height: 100.0,
+                          ),
                   ],
                 ),
               ],
